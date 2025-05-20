cadba385fcaff850dbb815d52399b25393f7760e
mateofilip <mateofernandofilip@gmail.com>
2025-05-20 18:41:48 -0300

    Fixed responsiveness to WeatherDetail component.

--- src/components/WeatherDetail.tsx
+++ src/components/WeatherDetail.tsx
@@ -39,12 +39,12 @@ export default function WeatherDetail({
       // data-blendy-to="card"
     >
       <div
-        className="relative flex h-[85vh] w-[85vw] flex-col place-items-center rounded-2xl bg-white px-3 py-5 dark:bg-slate-700"
+        className="relative flex h-[85vh] w-[85vw] flex-col place-items-center rounded-2xl bg-white px-3 py-5 lg:flex-row lg:p-10 dark:bg-slate-700"
         onClick={(e) => e.stopPropagation()}
       >
         <button
           onClick={() => setShowModal(false)}
-          className="absolute top-0 right-0 mt-3 mr-3 grid h-7 w-7 cursor-pointer place-items-center rounded-full bg-red-400 transition-all duration-300 ease-in-out hover:scale-110 hover:bg-red-600"
+          className="absolute top-0 right-0 z-50 mt-3 mr-3 grid h-7 w-7 cursor-pointer place-items-center rounded-full bg-red-400 transition-all duration-300 ease-in-out hover:scale-110 hover:bg-red-600"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -56,45 +56,44 @@ export default function WeatherDetail({
             <path d="M480-424 284-228q-11 11-28 11t-28-11q-11-11-11-28t11-28l196-196-196-196q-11-11-11-28t11-28q11-11 28-11t28 11l196 196 196-196q11-11 28-11t28 11q11 11 11 28t-11 28L536-480l196 196q11 11 11 28t-11 28q-11 11-28 11t-28-11L480-424Z" />
           </svg>
         </button>
-        <div className="flex h-1/2 flex-col place-items-center justify-center gap-3 text-center">
+        <div className="flex h-1/2 flex-col place-items-center justify-center gap-3 text-center lg:h-full lg:w-1/2 lg:gap-5">
           <img
             src={"/weather-icons/" + icon + ".png"}
-            className="w-1/3 drop-shadow-sm"
+            className="w-1/3 drop-shadow-sm md:w-1/4 lg:w-1/3"
             width="100"
             height="100"
             alt={weather}
           />
           <div>
-            <h3 className="text-3xl text-slate-700 dark:text-slate-100">
+            <h3 className="text-3xl text-slate-700 lg:text-5xl dark:text-slate-100">
               {name}
             </h3>
-            <h5 className="text-sm text-slate-500 dark:text-slate-400">
+            <h5 className="text-sm text-slate-500 lg:text-lg dark:text-slate-400">
               {weather}
             </h5>
           </div>
           <div className="flex flex-col gap-2">
-            <h5 className="text-5xl font-semibold text-slate-700 dark:text-slate-100">
+            <h5 className="text-5xl font-semibold text-slate-700 lg:text-7xl dark:text-slate-100">
               {Math.round(temperature)}°C
             </h5>
-            <p className="text-lg text-slate-600 dark:text-slate-400">
+            <p className="text-lg text-slate-600 lg:text-3xl dark:text-slate-400">
               {Math.round(min)}°C | {Math.round(max)}°C
             </p>
           </div>
         </div>
 
-        <div className="grid h-1/2 w-full grid-cols-2 grid-rows-3 place-items-center gap-2">
-          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 dark:bg-slate-800">
-            <div className="flex items-center gap-1 text-slate-700 dark:text-slate-100">
+        <div className="grid h-1/2 w-full grid-cols-2 grid-rows-3 place-items-center gap-2 lg:h-full lg:w-1/2 lg:gap-3">
+          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 shadow-md lg:p-5 dark:bg-slate-800">
+            <div className="flex items-center gap-1 text-slate-700 lg:text-xl dark:text-slate-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="1.5"
                 stroke-linecap="round"
                 stroke-linejoin="round"
+                className="h-6 w-6"
               >
                 <path d="M12 9a4 4 0 0 0-2 7.5" />
                 <path d="M12 3v2" />
@@ -107,22 +106,21 @@ export default function WeatherDetail({
                 Feels Like
               </h6>
             </div>
-            <p className="text-xl text-slate-600 dark:text-slate-400">
+            <p className="text-xl text-slate-600 lg:text-4xl dark:text-slate-400">
               {Math.round(feelsLike)}°C
             </p>
           </section>
-          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 dark:bg-slate-800">
-            <div className="flex items-center gap-1 text-slate-700 dark:text-slate-100">
+          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 shadow-md lg:p-5 dark:bg-slate-800">
+            <div className="flex items-center gap-1 text-slate-700 lg:text-xl dark:text-slate-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="1.5"
                 stroke-linecap="round"
                 stroke-linejoin="round"
+                className="h-6 w-6"
               >
                 <path d="M12.8 19.6A2 2 0 1 0 14 16H2" />
                 <path d="M17.5 8a2.5 2.5 0 1 1 2 4H2" />
@@ -132,22 +130,22 @@ export default function WeatherDetail({
                 Wind Speed
               </h6>
             </div>
-            <p className="text-xl text-slate-600 dark:text-slate-400">
+            <p className="text-xl text-slate-600 lg:text-4xl dark:text-slate-400">
+              {" "}
               {wind} m/s
             </p>
           </section>
-          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 dark:bg-slate-800">
-            <div className="flex items-center gap-1 text-slate-700 dark:text-slate-100">
+          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 shadow-md lg:p-5 dark:bg-slate-800">
+            <div className="flex items-center gap-1 text-slate-700 lg:text-xl dark:text-slate-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="1.5"
                 stroke-linecap="round"
                 stroke-linejoin="round"
+                className="h-6 w-6"
               >
                 <path d="M15.6 2.7a10 10 0 1 0 5.7 5.7" />
                 <circle cx="12" cy="12" r="2" />
@@ -157,22 +155,22 @@ export default function WeatherDetail({
                 Pressure
               </h6>
             </div>
-            <p className="text-xl text-slate-600 dark:text-slate-400">
+            <p className="text-xl text-slate-600 lg:text-4xl dark:text-slate-400">
+              {" "}
               {pressure} hPa
             </p>
           </section>
-          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 dark:bg-slate-800">
-            <div className="flex items-center gap-1 text-slate-700 dark:text-slate-100">
+          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 shadow-md lg:p-5 dark:bg-slate-800">
+            <div className="flex items-center gap-1 text-slate-700 lg:text-xl dark:text-slate-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="1.5"
                 stroke-linecap="round"
                 stroke-linejoin="round"
+                className="h-6 w-6"
               >
                 <path d="M7 16.3c2.2 0 4-1.83 4-4.05 0-1.16-.57-2.26-1.71-3.19S7.29 6.75 7 5.3c-.29 1.45-1.14 2.84-2.29 3.76S3 11.1 3 12.25c0 2.22 1.8 4.05 4 4.05z" />
                 <path d="M12.56 6.6A10.97 10.97 0 0 0 14 3.02c.5 2.5 2 4.9 4 6.5s3 3.5 3 5.5a6.98 6.98 0 0 1-11.91 4.97" />
@@ -181,22 +179,22 @@ export default function WeatherDetail({
                 Humidity
               </h6>
             </div>
-            <p className="text-xl text-slate-600 dark:text-slate-400">
+            <p className="text-xl text-slate-600 lg:text-4xl dark:text-slate-400">
+              {" "}
               {humidity}%
             </p>
           </section>
-          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 dark:bg-slate-800">
-            <div className="flex items-center gap-1 text-slate-700 dark:text-slate-100">
+          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 shadow-md lg:p-5 dark:bg-slate-800">
+            <div className="flex items-center gap-1 text-slate-700 lg:text-xl dark:text-slate-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="1.5"
                 stroke-linecap="round"
                 stroke-linejoin="round"
+                className="h-6 w-6"
               >
                 <path d="M12 2v8" />
                 <path d="m4.93 10.93 1.41 1.41" />
@@ -211,25 +209,25 @@ export default function WeatherDetail({
                 Sunrise
               </h6>
             </div>
-            <p className="text-xl text-slate-600 dark:text-slate-400">
+            <p className="text-xl text-slate-600 lg:text-4xl dark:text-slate-400">
+              {" "}
               {sunriseTime.toLocaleTimeString([], {
                 hour: "2-digit",
                 minute: "2-digit",
               })}
             </p>
           </section>
-          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 dark:bg-slate-800">
-            <div className="flex items-center gap-1 text-slate-700 dark:text-slate-100">
+          <section className="grid h-full w-full content-between rounded-2xl bg-slate-100 p-3 opacity-85 shadow-md lg:p-5 dark:bg-slate-800">
+            <div className="flex items-center gap-1 text-slate-700 lg:text-xl dark:text-slate-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="1.5"
                 stroke-linecap="round"
                 stroke-linejoin="round"
+                className="h-6 w-6"
               >
                 <path d="M12 10V2" />
                 <path d="m4.93 10.93 1.41 1.41" />
@@ -244,7 +242,8 @@ export default function WeatherDetail({
                 Sunset
               </h6>
             </div>
-            <p className="text-xl text-slate-600 dark:text-slate-400">
+            <p className="text-xl text-slate-600 lg:text-4xl dark:text-slate-400">
+              {" "}
               {sunsetTime.toLocaleTimeString([], {
                 hour: "2-digit",
                 minute: "2-digit",
