64ddbc7419e29fc92dacd2004badc5c13fb36ed6
mateofilip <mateofernandofilip@gmail.com>
2026-08-07 18:27:28 -0300

    Many, many fixes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -80,16 +80,6 @@ export default function Home() {
   }
 
   useEffect(() => {
-    const selectedTheme = localStorage.getItem("theme");
-
-    if (selectedTheme) {
-      document.body.classList.add(selectedTheme);
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.add("light");
-    }
-
     const savedPinned = localStorage.getItem("pinnedCities");
     if (savedPinned) {
       try {
